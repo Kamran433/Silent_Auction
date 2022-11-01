8e822a4db7394ca95a58b21cae1961e2be351a89
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:15:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# import art_au
+# from replit import clear
+# def get_key(val):
+#  for key, value in dict.items():
+#    if val==value:
+#      return key
+# print(art_au.logo)
+# print("Welcome to the secret auction program.")
+# bidding=True
+# dict={}
+# max=0
+# while bidding is True:
+#  person=input("What is your name? : ")
+#  bid=input("What's your bid? : $")
+#  dict[person]=bid
+#  perm=input("Are there any other bidders? Type 'yes' or 'no'.")
+#  clear()
+#  if perm=="yes":
+#    bidding=True
+#  elif perm=="no":
+#    bidding=False
+# for m in dict:
+#   j=int(dict[m])
+#   if j>=max:
+#      max=j
+# clear()
+# men=get_key(str(max))
+# print(f"The winner is {men} with a bid of ${max}.")
